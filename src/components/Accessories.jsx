b339c875540e3101eeb68fd9a079be150487b8ee
harshal-1 <119671464+harshal-1@users.noreply.github.com>
2024-11-25 11:00:36 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Accessories.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Accessories() {
+
+  return (
+    <div>
+      Accessories
+    </div>
+  )
+}
+
---
+++ src/components/AccessoryPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function AccessoryPage() {
+  
+  return (
+    <div>
+      Accessories
+    </div>
+  )
+}
+
